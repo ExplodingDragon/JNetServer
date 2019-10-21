caec817974e724dfb33e1a2d43dc3e73e8f18e0b
Noob <ExplodingFKL@gmail.com>
2019-10-21 19:00:20 +0800

    修复一个小问题.

--- Simple/src/main/java/top/fksoft/server/Main.java
+++ Simple/src/main/java/top/fksoft/server/Main.java
@@ -11,10 +11,12 @@ public class Main extends Application {
     private static Logger logger = Logger.getLogger(Main.class);
     public static void main(String[] args) {
         logger.info("项目的范例已经启动 ！");
+        launch(args);
+
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        stage.show();
     }
 }
