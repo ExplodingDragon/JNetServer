3a7b0ece3567eba5738ba7fc41b70359eb56f780
Noob <ExplodingFKL@gmail.com>
2019-10-21 18:56:51 +0800

    修改范例位置。

---
+++ Simple/src/main/java/top/fksoft/server/Main.java
@@ -0,0 +1,20 @@
+package top.fksoft.server;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import jdkUtils.logcat.Logger;
+
+/**
+ * @author Explo
+ */
+public class Main extends Application {
+    private static Logger logger = Logger.getLogger(Main.class);
+    public static void main(String[] args) {
+        logger.info("项目的范例已经启动 ！");
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
